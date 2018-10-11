15151b45f2e1c6d8ecec723fd9af832359832ac5
mbanda1 <mbandanixon@gmail.com>
2018-10-11 12:25:01 +0300

    use local database mongodb : 127.0.0.1

--- db.js
+++ db.js
@@ -1,14 +1,7 @@
 const mongoose = require('mongoose');
 //mongoose.connect('mongodb://yourMongoDBURIGoesHere');
-// mongoose.connect('mongodb://localhost:27017/Biashara-1');
+ mongoose.connect('mongodb://localhost:27017/Biashara-1');
 
  //mongoose.connect('mongodb://mbanda:mbanda@ds115740.mlab.com:15740/biashara-point')
 
- 
-  mongoose.connect('mongodb://mbanda1:mbanda1@ds115740.mlab.com:15740/biashara-point', function(err, db) {
-    if (err) {
-        console.log('Unable to connect :', err);
-    } else {
-        console.log('Connected to Server successfully!');
-    }
-}); 
+  
