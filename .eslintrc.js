29111630e32c81fc84671e36fc98eaa4b2aef40c
mbandanixon <mbandanixon@gmail.com>
2019-03-08 11:03:19 +0300

    linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,26 @@ module.exports = {
         "ecmaVersion": 2018
     },
     "rules": {
+        "no-fallthrough": 2,
+        "no-unreachable": 2,
+        "no-cond-assign": 2,
+        "no-console": 2,
+        "valid-typeof": 2,
+        "no-func-assign": 2,
+        "no-extra-semi": 2, //cool
+       
+        "no-undef": 2,
+        "no-empty": 2,
+        "no-case-declarations": 0,
+        "no-mixed-spaces-and-tabs": 0,
+        "no-useless-escape": 2,
+
+        "no-empty-function":1,
+        "no-redeclare":2,
+        "no-unused-vars":1,
+        "prefer-promise-reject-errors":0,
+        "no-constant-condition":0,
+        "no-unused-expressions":0,
+        
     }
 };
